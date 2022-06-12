838b49e1b4f443f3df13586cb0b85add35e6d18e
supitsara prathan <supitsara@digio.co.th>
2022-06-12 15:34:32 +0700

    26. Remove Duplicates from Sorted Array

---
+++ src/test/java/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RemoveDuplicatesFromSortedArrayTest {
+    @Test
+    void testcase1() {
+        int[] nums = {1,1,2};
+        assertEquals(2, RemoveDuplicatesFromSortedArray.removeDuplicates(nums));
+    }
+    @Test
+    void testcase2() {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        assertEquals(5, RemoveDuplicatesFromSortedArray.removeDuplicates(nums));
+    }
+}
